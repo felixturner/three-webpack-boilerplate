b83b73c3e2793b4a4cbb2857f8982b49ab9eb62a
felix turner <felixturner@gmail.com>
2020-01-30 10:21:28 -0800

    load stats from three.js jsm. update deps

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import Stats from 'stats.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 /*
 
@@ -11,6 +11,7 @@ import Stats from 'stats.js';
 let scene, camera, renderer, cube, controls, stats;
 
 function init() {
+	console.log('THREE v', THREE.REVISION);
 	stats = new Stats();
 	document.body.appendChild(stats.dom);
 
